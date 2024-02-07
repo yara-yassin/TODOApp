685f91b16b11c37f40ad8153d00a1701b09e6c7a
Yara <yarayassin1611@gmail.com>
2024-02-08 01:40:12 +0200

    update

--- src/Redux/slices/todoSlice.js
+++ src/Redux/slices/todoSlice.js
@@ -5,23 +5,34 @@ const todoSlice = createSlice({
   name: "todo",
   initialState: {
     todos: [],
+    completedTodos: [],
   },
   reducers: {
     addTodo: (state, action) => {
       const { title, description } = action.payload;
-      const existingTodo = state.todos.find(
-        (todo) => todo.title === title && todo.description === description
-      );
-
-      if (!existingTodo) {
-        state.todos.push({
-          id: Date.now().toString(),
-          title,
-          description,
-          completed: false,
-        });
+      if (!title.trim() && !description.trim()) {
+        alert("Title and Description cannot be empty!");
+      } else if (!title.trim() && description.trim()) {
+        alert("Title cannot be empty!");
+      } else if (title.trim() && !description.trim()) {
+        alert("Description cannot be empty!");
       } else {
-        console.warn("Todo already exists!");
+        const existingTodo = state.todos.find(
+          (todo) =>
+            todo.title.toLowerCase() === title.toLowerCase() &&
+            todo.description.toLowerCase() === description.toLowerCase()
+        );
+
+        if (!existingTodo) {
+          state.todos.push({
+            id: Date.now().toString(),
+            title,
+            description,
+            completed: false,
+          });
+        } else {
+          console.warn("Todo already exists!");
+        }
       }
     },
     deleteTodo: (state, action) => {
@@ -31,8 +42,8 @@ const todoSlice = createSlice({
     complete: (state, action) => {
       const todoId = action.payload;
       state.todos = state.todos.map((todo) =>
-        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
-      );
+      todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+    );
     },
   },
 });
